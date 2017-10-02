e7aef126ed2fe6602f3e75f2f94da1bfb09ab769
Fedor Ilinykh <ify@kibibox.com>
2017-10-03 02:47:53 +0600

    models

--- src/main/java/org/statesync/model/AnnotatedList.java
+++ src/main/java/org/statesync/model/AnnotatedList.java
@@ -1,11 +1,14 @@
 package org.statesync.model;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import lombok.Data;
 
 @Data
 public class AnnotatedList<Item> {
-	public AnnotatedList<Item> data;
-	public Map<String, Permission> permissions;
+	public List<AnnotatedItem<Item>> data = new ArrayList<>();
+	public Map<Permission, Boolean> permissions = new HashMap<>();
 }
